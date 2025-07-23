beef193acb8b24dcbfc98a06f2889a6e18dd9c2e
samyachd <achd.samy@gmail.com>
2025-07-23 13:50:23 +0200

    update

--- database/insert_data.py
+++ database/insert_data.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def insert_to_database(file: str,name: str):
     df_books = pd.read_csv(file)
-    connection = sqlite3.connect(name+".db")
+    connection = sqlite3.connect("database/"+name+".db")
     df_books.to_sql("books",connection, if_exists="replace")
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM books")
